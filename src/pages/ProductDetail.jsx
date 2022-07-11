e9cdf01819806bbe6c838d860b4cbdf7ade39c3a
CodeSocrates <furo9899@naver.com>
2022-07-11 12:37:16 +0900

    test: 오류로 인한 상품 디테일 페이지 기본 대응 로직 제거

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -2,11 +2,14 @@ import DetailNavBar from '../components/DetailNavBar';
 import { useParams } from "react-router-dom"
 import { useEffect } from 'react';
 import { getProductDetail } from '../data/mockData';
+import { useState } from 'react';
 
-const ProductDetail = ()=>{
+const ProductDetail = () => {
+  //url에서 파라미터 변수(productId) 받아오는 로직
   let { productId } = useParams();
   const [product, setProduct] = useState();
 
+  //형제 데이터를 가져온다음에 스테이트에 담아줌
   useEffect(() => {
     const result = getProductDetail(productId);
     setProduct(result);
@@ -14,7 +17,16 @@ const ProductDetail = ()=>{
 
   return (
       <DetailNavBar />
+      
+      /*{
+        product && (
+        <>
+          <div>상품 ID : {product.id}</div>
+          <img src={product.thumbnail} width="200" />
+          <div>{product.name}</div>
+          <div>{product.description}</div>
+        </>
+      )}*/
   );
 }
-
 export default ProductDetail;
